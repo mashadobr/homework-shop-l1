bc5a07bd5b6e14b6bf7de394a7b241bb8a8d1e45
Добровольская Маша <dobrbobr89@gmail.com>
2025-08-04 13:10:49 +0400

    homework onlain shop lessons 7

--- src/org/skypro/skyshop/product/Product.java
+++ src/org/skypro/skyshop/product/Product.java
@@ -2,11 +2,12 @@ package org.skypro.skyshop.product;
 
 import org.skypro.skyshop.Searchable.Searchable;
 
+import java.util.Objects;
+
 public abstract class Product implements Searchable {
     private final String name;
 
     public Product(String name) {
-
         if (name == null || name.isBlank()) {
             throw new IllegalArgumentException("Имя пользователя не может быть пустым");
         }
@@ -30,5 +31,17 @@ public abstract class Product implements Searchable {
     @Override
     public String getContentType() {
         return "PRODUCT";
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Product product = (Product) o;
+        return Objects.equals(name, product.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(name);
     }
 }
